b9ec1c37cdae16612f52b76f18e8785cb18f816d
Brandon Teich <brandonteich123@gmail.com>
2018-09-20 00:26:18 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,5 +1,5 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(arg1="Miles", arg2="Coltrane", arg3="Roach")
+def parrot(output)
   "#{arg1}, #{arg2}, #{arg3}."
 end
