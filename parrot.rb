498b72ba96da0b2949717f0b1815e7f0f0ca31d6
Johnathon Wood <jaywood128@gmail.com>
2018-10-14 18:55:24 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,7 @@
-# Create method `parrot` that outputs a given phrase and
+def parrot("phrase", default = "Squawk")
+  puts "phrase"
+  phrase
+end
+
+parrot('Hello')  # Create method `parrot` that outputs a given phrase and
 # returns the phrase
