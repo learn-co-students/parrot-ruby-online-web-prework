e585b80d5e62f04ee96eeb7fa71874c80314718a
Robert McCary <robert.mccary@yahoo.com>
2018-11-23 22:07:55 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,5 @@
-# Create method `parrot` that outputs a given phrase and
-# returns the phrase
+def parrot(phrase = "Squawk!")# Create method `parrot` that outputs a given phrase and
+puts "#{phrase}"
+
+return "#{phrase}"
+end
