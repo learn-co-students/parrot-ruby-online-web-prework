d70cbf295bb0c35dc9c96deefb1d2b9497256c0e
Arianna Kosten <arianna.kosten@gmail.com>
2019-04-14 19:01:54 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,8 @@
-# Create method `parrot` that outputs a given phrase and
-# returns the phrase
+def parrot(phrase ="Squawk!")
+  puts phrase
+  if phrase == "Pretty bird!"
+    return "Pretty bird!"
+  else
+    return "Squawk!"
+  end
+end
